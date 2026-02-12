47f9473bfb0ab4292b598503eb1f59854f2e0636
Niloy Gope <ngcomputer1517@gmail.com>
2026-02-12 16:01:03 +0600

    change userSlice

--- frontend/src/features/user/userSlice.js
+++ frontend/src/features/user/userSlice.js
@@ -4,7 +4,7 @@ import { createSlice } from "@reduxjs/toolkit"
 const userSlice = createSlice({
     name: 'user',
     initialState: {
-        userData: {}
+        userData: null
     },
     reducers: {
         setUserData: (state, action) => {
